62b4099607862ec35a00c022c39bc171c15da65c
david <orbinxproductions@gmail.com>
2026-04-25 17:49:46 -0400

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,7 @@ const defaultConfig = {
 
 if (typeof window !== "undefined") {
 	var existingConfig = window._CONFIG && typeof window._CONFIG === "object" ? window._CONFIG : {};
-	window._CONFIG = Object.assign({}, defaultConfig, existingConfig);
-	if (!window.WISP_URL && window._CONFIG.WISP_URL) {
-		window.WISP_URL = window._CONFIG.WISP_URL;
-	}
+	window._CONFIG = Object.assign({}, existingConfig, defaultConfig);
+	window._CONFIG.WISP_URL = defaultConfig.WISP_URL;
+	window.WISP_URL = defaultConfig.WISP_URL;
 }
